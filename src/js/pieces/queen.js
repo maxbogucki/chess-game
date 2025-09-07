d3e9f000a90fa8cfe1d1dda4c6fac62ee520fc96
maxbogucki <bogucki.max@gmail.com>
2025-09-07 13:18:29 -0500

    implemented basic queen.js legal moves

--- src/js/pieces/queen.js
+++ src/js/pieces/queen.js
@@ -1,4 +1,9 @@
 import Piece from "./piece.js";
+import {
+  getVerticalMoves,
+  getHorizontalMoves,
+  getDiagonalMoves,
+} from "../utils/moveHelpers.js";
 
 export default class Queen extends Piece {
   constructor(square, color) {
@@ -7,5 +12,13 @@ export default class Queen extends Piece {
 
   getSymbol() {
     return this.color === "white" ? "♕" : "♛";
+  }
+
+  getLegalMoves(board) {
+    return [
+      ...getVerticalMoves.call(this, board),
+      ...getHorizontalMoves.call(this, board),
+      ...getDiagonalMoves.call(this, board),
+    ];
   }
 }
